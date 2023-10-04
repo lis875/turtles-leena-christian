e0d3f0aa0408d8b2975e8411171b9da6367d83da
Leena <leena@wisc.edu>
2023-10-03 23:17:19 -0500

    Exercise7&8

--- star.py
+++ star.py
@@ -1,5 +1,19 @@
-from turtle import *
+'''from turtle import *
 for _ in range(5):
     forward(100)  # Move forward by 100 units
     right(144)    # Turn right by 144 degrees
+done()
+'''
+from turtle import *
+
+for _ in range(5):
+    # Loop to draw a single star
+    for _ in range(5):
+        forward(100)  # Move forward by 100 units
+        right(144)    # Turn right by 144 degrees
+    
+    # Move the turtle to the starting position for the next star
+    penup()
+    forward(150)  # Move forward to create space between stars
+    pendown()
 done()
