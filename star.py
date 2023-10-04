21db6a2427b424edde2707c78b3bedcccb87674a
Leena <leena@wisc.edu>
2023-10-03 23:20:09 -0500

    ot Exercise7,8&9

--- star.py
+++ star.py
@@ -4,7 +4,7 @@ for _ in range(5):
     right(144)    # Turn right by 144 degrees
 done()
 '''
-from turtle import *
+'''from turtle import *
 
 for _ in range(5):
     # Loop to draw a single star
@@ -16,4 +16,18 @@ for _ in range(5):
     penup()
     forward(150)  # Move forward to create space between stars
     pendown()
+done()
+'''
+
+from turtle import *
+star_colors = ["red","green","blue", "pink", "lime"]
+
+# Loop to draw and fill the stars with different colors
+for color in star_colors:
+    begin_fill()  # Start filling
+    fillcolor(color)  # Set the fill color
+    for _ in range(5):
+        forward(100)  # Move forward by 100 units
+        right(144)    # Turn right by 144 degrees
+    end_fill()  # End filling
 done()
