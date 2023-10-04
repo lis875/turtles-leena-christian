80de0d6e924eb36d4c6c43133525a9fbb9621e64
Leena <leena@wisc.edu>
2023-10-03 23:09:57 -0500

    Exercise 5

---
+++ guestbook_leena.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+name = textinput("Name","Enter your name here: ")
+
+write(name)
+
+penup()
+right(90)
+forward(50)
+left(90)
+pendown()
+
+name2 = textinput("Second Name", "Enter the Second name here: ")
+
+write(name2)
+
+penup()
+right(90)
+forward(50)
+left(90)
+pendown()
+
+done()
